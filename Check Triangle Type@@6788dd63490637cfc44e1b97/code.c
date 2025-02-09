97fa21b5855a9e7121c0776dcbd9b3ce44b84ae1
vidhiiupadhyay <vidhi1776.becse24@chitkara.edu.in>
2025-02-09 18:48:47 +0530

    Solution to Check Triangle Type

--- Check Triangle Type@@6788dd63490637cfc44e1b97/code.c
+++ Check Triangle Type@@6788dd63490637cfc44e1b97/code.c
@@ -3,7 +3,7 @@
 int main() {
     int a,b,c;
     scanf("%d %d %d",&a,&b,&c);
-    if(a=b && b=c && a=c){
+    if((a=b) && (b=c) && (a=c)){
         printf("Equilateral");
     }
     else if(a!=b!=c){
